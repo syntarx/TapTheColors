99f0f81a6c3aff490f48bcb12c909ede7eac2957
Andri <andri.camenisch@epost.ch>
2024-01-15 08:45:29 +0100

    Project created, first layout done, Git repo start

---
+++ app/src/main/java/com/example/tapthecolors/SpielActivity.java
@@ -0,0 +1,14 @@
+package com.example.tapthecolors;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SpielActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_spiel);
+    }
+}
