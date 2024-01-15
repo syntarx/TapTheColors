f4d5674c2b1adabb801fb09313403ce39c588e94
Andri <andri.camenisch@epost.ch>
2024-01-15 09:33:18 +0100

    layout fuer main und spiel in einer ersten Version fertig gestellt, InfoActivity zum Projekt zugefuegt

--- app/src/main/java/com/example/tapthecolors/InfoActivity.java
+++ app/src/main/java/com/example/tapthecolors/InfoActivity.java
@@ -1,13 +1,14 @@
 package com.example.tapthecolors;
 
 import androidx.appcompat.app.AppCompatActivity;
+
 import android.os.Bundle;
 
-public class SpielActivity extends AppCompatActivity {
+public class InfoActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_spiel);
+        setContentView(R.layout.activity_info);
     }
 }
