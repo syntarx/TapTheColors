2c10addc7941e356083d6379d8382d300efba120
Andri <andri.camenisch@epost.ch>
2024-01-15 15:38:33 +0100

    ColorGenerator von der Logik  her fertig und in der SpielActivity implementiert

--- app/src/main/java/com/example/tapthecolors/SpielActivity.java
+++ app/src/main/java/com/example/tapthecolors/SpielActivity.java
@@ -4,28 +4,51 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
+import com.example.tapthecolors.services.ColorGenerator;
+
+import java.lang.reflect.Array;
+
 public class SpielActivity extends AppCompatActivity {
 
-    Button test;
+    Button[] buttons = new Button[9];
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_spiel);
 
-        test = findViewById(R.id.button);
+        buttons[0] = findViewById(R.id.button);
+        buttons[1] = findViewById(R.id.button2);
+        buttons[2] = findViewById(R.id.button3);
+        buttons[3] = findViewById(R.id.button4);
+        buttons[4] = findViewById(R.id.button5);
+        buttons[5] = findViewById(R.id.button6);
+        buttons[6] = findViewById(R.id.button7);
+        buttons[7] = findViewById(R.id.button8);
+        buttons[8] = findViewById(R.id.button9);
+
+        ColorGenerator colorGenerator = new ColorGenerator();
+
+        for (int i = 0; i < buttons.length; i++) {
+            String farbe = colorGenerator.Farbe();
+            Log.println(Log.DEBUG,"debugging", String.valueOf(farbe));
+            buttons[i].setBackgroundColor(Color.parseColor(farbe));
+        }
+
 
         Intent gameOverActivity = new Intent(SpielActivity.this, GameOverActivity.class);
 
-        test.setOnClickListener(new View.OnClickListener() {
+/*        option1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 startActivity(gameOverActivity);
             }
-        });
+        });*/
     }
 }
--- app/src/main/java/com/example/tapthecolors/services/ColorGenerator.java
+++ app/src/main/java/com/example/tapthecolors/services/ColorGenerator.java
@@ -1,2 +1,18 @@
-package com.example.tapthecolors.services;public class ColorGenerator {
+package com.example.tapthecolors.services;
+
+import java.util.Random;
+
+public class ColorGenerator {
+    public String Farbe(){
+        Random random = new Random();
+
+        int nextInt = random.nextInt(0xffffff + 1);
+
+        String colorCode = String.format("#%06x", nextInt);
+
+        return colorCode;
+    }
 }
+
+
+// https://stackoverflow.com/questions/35459454/java-random-color-string
