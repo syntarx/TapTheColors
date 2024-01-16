bd0f76363e26d3c9526269937c3b1d5adc900ba5
Andri <andri.camenisch@epost.ch>
2024-01-16 15:35:42 +0100

    Notifications und Vibration implementiert, Readme erstellt

--- app/src/main/java/com/example/tapthecolors/SpielActivity.java
+++ app/src/main/java/com/example/tapthecolors/SpielActivity.java
@@ -2,13 +2,20 @@ package com.example.tapthecolors;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.core.app.NotificationCompat;
 
 import android.annotation.SuppressLint;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.content.Context;
 import android.content.Intent;
 import android.graphics.Color;
 import android.graphics.drawable.Drawable;
 import android.net.wifi.p2p.WifiP2pManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.VibrationEffect;
+import android.os.Vibrator;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -22,10 +29,39 @@ public class SpielActivity extends AppCompatActivity {
 
     Button[] buttons = new Button[9];
 
+    private NotificationManager notificationManager;
+    private static final String CHANNEL_ID = "defaultChannel";
+    private static final String CHANNEL_NAME = "Default Channel";
+
+    private void sendNotification() {
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this, CHANNEL_ID)
+                .setSmallIcon(android.R.drawable.ic_media_play)
+                .setContentTitle("Game Over!")
+                .setContentText("Du hast verloren, hahhahahahahahaha!")
+                .setPriority(NotificationCompat.PRIORITY_HIGH);
+        notificationManager.notify(1, builder.build());
+    }
+
+    private void vibrate() {
+        Vibrator vibrator = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
+        if (vibrator != null && vibrator.hasVibrator()) {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                vibrator.vibrate(VibrationEffect.createOneShot(500, VibrationEffect.DEFAULT_AMPLITUDE));
+            }
+        }
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_spiel);
+
+        this.notificationManager = (NotificationManager) this.getSystemService(Context.NOTIFICATION_SERVICE);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            NotificationChannel channel = new NotificationChannel(CHANNEL_ID, CHANNEL_NAME, NotificationManager.IMPORTANCE_HIGH);
+            notificationManager.createNotificationChannel(channel);
+        }
+
 
         buttons[0] = findViewById(R.id.button);
         buttons[1] = findViewById(R.id.button2);
@@ -56,6 +92,7 @@ public class SpielActivity extends AppCompatActivity {
                 buttons[i].setOnClickListener(new View.OnClickListener() {
                     @Override
                     public void onClick(View v) {
+                        vibrate();
                         startActivity(spielActivity);
                     }
                 });
@@ -63,6 +100,7 @@ public class SpielActivity extends AppCompatActivity {
                 buttons[i].setOnClickListener(new View.OnClickListener() {
                     @Override
                     public void onClick(View v) {
+                        sendNotification();
                         startActivity(gameOverActivity);
                     }
                 });
